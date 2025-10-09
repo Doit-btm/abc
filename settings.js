323cc73739fcb0803a1280a1b590fe2bafa1a084
Doit-btm <m96791318@gmail.com>
2025-10-09 20:51:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
+    return text === fault ? true : false;}
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=09151982f407de60' : process.env.SESSION_ID,
